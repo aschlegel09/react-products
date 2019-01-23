2ac4e212708cfb1413356dd7e033970d9c5504a2
Andrew Schlegel <aschlegel09@gmail.com>
2019-01-23 18:04:22 -0500

    heroku create app

--- product-app/client/src/components/Cart/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const selectUser = (user) => {
-    console.log("you clicked on: ", user.name);
-    return {
-        type: "USER_SELECTED",
-        payload: user
-    }
-};
--- product-app/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const productRoutes = require("./product");
-const userRoutes = require("./user");
-
-// Product routes
-router.use("/products", productRoutes);
-router.use("/profile", userRoutes)
-
-module.exports = router;
