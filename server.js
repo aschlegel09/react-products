6d487283535e7ade6df4ce23cec82181d45c58f2
Andrew Schlegel <aschlegel09@gmail.com>
2019-01-23 20:54:37 -0500

    error checking

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/build"));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
+   res.sendFile(path.join(__dirname, '/client/build/index.html'))
   });
 }
 //  Serve static files
