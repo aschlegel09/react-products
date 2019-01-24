f8aff275d94fa02ec75850c1ad6074331038c8b4
Andrew Schlegel <aschlegel09@gmail.com>
2019-01-23 20:52:43 -0500

    error checking

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.join('build', 'index.html'));
   });
